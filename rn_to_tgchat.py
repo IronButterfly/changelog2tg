84a143b5eced1bf0c58ca2132700f02139282004
Igor Gubaidullin <scottman@yandex.ru>
2024-04-18 14:22:20 +0700

    Update rn_to_tgchat.py

--- rn_to_tgchat.py
+++ rn_to_tgchat.py
@@ -6,17 +6,17 @@ import time
 
 TOKEN = 'YOUR_BOT_TOKEN'
 CHAT_ID = 'YOUR_CHAT_ID'
-URL = 'https://www.ispmanager.ru/changelog'
+URL = 'https://www.your_domain.tld/changelog'
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Бот запущен. Ожидайте сообщений...')
+    update.message.reply_text('Bot is up and running. Expect messages...')
 
 def get_release_notes() -> str:
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Здесь вы должны указать, как найти Release Notes на веб-странице.
-    # Например, если Release Notes находятся в теге <p> с классом 'release-notes':
+    # Here you must specify how to find Release Notes on the web page.
+    #  For example, if Release Notes are in a <p> tag with the class 'release-notes':
     release_notes = soup.find('div', class_='js-changelog-item')
 
     return release_notes.text if release_notes else None
@@ -41,7 +41,7 @@ def main() -> None:
             publish_release_notes(updater.bot, release_notes)
             last_release_notes = release_notes
 
-        time.sleep(60*60)  # Проверяем каждый час
+        time.sleep(60*60)  # Checking every hour
 
     updater.idle()
 
